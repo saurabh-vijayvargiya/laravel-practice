6bd7f6e16f63b3864261bcfd7062cf3847bd5c6c
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2019-04-18 00:06:04 +0800

    Added post route for the submission data saving

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     $links = \App\Link::all();
     return view('welcome', ['links' => $links]);
@@ -22,4 +24,17 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/submit', function () {
     return view('submit');
+});
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+      'title' => 'required|max:255',
+      'url' => 'required|url|max:255',
+      'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new \App\Link($data))->save();
+
+    return redirect('/');
+
 });
